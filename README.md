fb7776b697b12654ce86d80943f4778bafa80345
melissa <nadia.mm.mariduena@gmail.com>
2020-09-05 21:16:29 +0200

    readme update

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 #### gallery intro
+
+## Beginner intro to json data (part1) | react
+
+~~No copyright infringement intended, All pictures used in this exercise are **_only_** for educational purposes.~~
+
+![preview](../public/1b.jpg)
---
+++ build/precache-manifest.05697de030e6ce09a04bf4c60cc5aee9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "da331e1874122246d4b303f4c8609fb2",
+    "url": "/react-json-version1/index.html"
+  },
+  {
+    "revision": "392e031130c698e22078",
+    "url": "/react-json-version1/static/css/main.8d425b5a.chunk.css"
+  },
+  {
+    "revision": "1a894ffa880be03de6be",
+    "url": "/react-json-version1/static/js/2.76dc16bd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-json-version1/static/js/2.76dc16bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "392e031130c698e22078",
+    "url": "/react-json-version1/static/js/main.b8ef09cc.chunk.js"
+  },
+  {
+    "revision": "0d6c0d2e72e57dabf59f",
+    "url": "/react-json-version1/static/js/runtime-main.f2b9106b.js"
+  }
+]);
